17f51d2ad1f2180dc5d987bb8ba6f1b310d99fe1
emperWang <544094478@qq.com>
2019-05-20 13:34:58 +0800

    base java implement mybatis generate

---
+++ MyBatis-Generator-java2/src/com/wk/generate/GeneratorSqlmap.java
@@ -0,0 +1,37 @@
+package com.wk.generate;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        URL resource = Thread.currentThread().getContextClassLoader().getResource("generatorConfig.xml");
+        // 指定配置文件
+        File configFile = new File(resource.getPath());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    // 执行main方法以生成代码
+    public static void main(String[] args) {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
