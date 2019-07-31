490d1926fb3f3f2e3ddc78882920072b061ec99e
emperwang <544094478@qq.com>
2019-07-31 09:51:35 +0800

    线程处理socket请求

---
+++ JavaBaseProject/src/main/java/com/wk/socket/SocketServerThread.java
@@ -0,0 +1,55 @@
+package com.wk.socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServerThread {
+    private  static  final Logger logger = LoggerFactory.getLogger(SocketServerdemo.class);
+    private static final String CharSet = "UTF-8";
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9001);
+        while (true){
+            Socket socket = serverSocket.accept();
+            new processSocket(socket).start();
+        }
+    }
+
+    static class processSocket extends Thread{
+
+        private Socket socket;
+
+        public processSocket(Socket socket){
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                // 接收客户端发过来的数据
+                InputStream inputStream = socket.getInputStream();
+                int count = 0;
+                while (count == 0){
+                    count = inputStream.available();
+                }
+                byte[] bytes = new byte[count];
+                inputStream.read(bytes);
+                logger.info("receive msg is {}",new String(bytes));
+                // 向客户端发送数据
+                OutputStream outputStream = socket.getOutputStream();
+                byte[] data = "hello, welcome to wk server!".getBytes();
+                outputStream.write(data);
+                outputStream.flush();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
