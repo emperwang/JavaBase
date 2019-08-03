9b289f6250f184b7da470e0ed4a7be8148cca876
emperWang <544094478@qq.com>
2019-08-03 12:11:23 +0800

    CGLIB 代理类生成

--- JavaBaseProject/src/main/java/com/wk/proxy/CglibProxy/TestMain.java
+++ JavaBaseProject/src/main/java/com/wk/proxy/CglibProxy/TestMain.java
@@ -1,9 +1,11 @@
 package com.wk.proxy.CglibProxy;
 
 import com.wk.proxy.API.MathImpl;
+import net.sf.cglib.core.DebuggingClassWriter;
 
 public class TestMain {
     public static void main(String[] args) {
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "target/cglib");
         CglibProxy cglibProxy = new CglibProxy();
         MathImpl proxyObject = (MathImpl) cglibProxy.getProxyObject(new MathImpl());
         proxyObject.add(1,2);
