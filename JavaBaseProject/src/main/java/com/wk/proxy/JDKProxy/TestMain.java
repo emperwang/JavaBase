5b879423162abc7c334607d15fef16cd89fbaf5e
emperWang <544094478@qq.com>
2019-08-03 11:54:55 +0800

    生成代理类配置

--- JavaBaseProject/src/main/java/com/wk/proxy/JDKProxy/TestMain.java
+++ JavaBaseProject/src/main/java/com/wk/proxy/JDKProxy/TestMain.java
@@ -5,6 +5,7 @@ import com.wk.proxy.API.MathImpl;
 
 public class TestMain {
     public static void main(String[] args) {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
         JDKProxy jdkProxy = new JDKProxy();
         MathCalc mathCalc = (MathCalc) jdkProxy.getProxyObject(new MathImpl());
         mathCalc.add(1,2);
