48fbbce7cb91e3a0f8ea9a325049a21fa3f07411
emperwang <544094478@qq.com>
2019-08-05 19:26:17 +0800

    add comment

--- MybatisSource/src/main/java/com/mybatis/MainStarter/MainApp.java
+++ MybatisSource/src/main/java/com/mybatis/MainStarter/MainApp.java
@@ -8,6 +8,15 @@ import org.apache.ibatis.session.SqlSessionFactory;
 
 import java.util.List;
 
+/**
+ *  1. 配置文件配置流程
+ *  2. 接口和类的对应关系
+ *  3. 一个接口执行的流程(接口找到对应的sql语句)
+ *  4. 参数的配置
+ *  5. 连接池
+ *  6. 事务管理
+ *  7. 结果如何映射到javabean中
+ */
 public class MainApp {
     public static void main(String[] args) {
         SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
