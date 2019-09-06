047ab23266c43713b6ba2ae2da4cf92170e7ea9b
emperWang <544094478@qq.com>
2019-09-06 18:50:15 +0800

    test return

--- JavaBaseProject/src/main/java/com/wk/Test/returnUse/TestReturn.java
+++ JavaBaseProject/src/main/java/com/wk/Test/returnUse/TestReturn.java
@@ -15,10 +15,33 @@ public class TestReturn {
             return 3;
         }
     }
-
-    public static void main(String[] args) {
-
+    public static int test2(){
+        try{
+            throw new Exception("something wrong");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return 2;
+        }
+    }
+    public static int test3(){
+        int a = 0;
+        try{
+            a++;
+            return a;
+        } finally {
+            ++a;
+            log.info("a = "+a);
+        }
     }
 
+    public static void main(String[] args) {
+        int i = test1();
+        log.info("i is :"+i);
 
+        int i1 = test2();
+        log.info("res is:"+i1);
+
+        int i2 = test3();
+        log.info("res is:"+i2);
+    }
 }
