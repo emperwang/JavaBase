a67287f6418228ef77d002a0e209901e7e0e4ec9
emperwang <544094478@qq.com>
2019-09-10 20:13:18 +0800

    ch7 model MBean, something wrong

---
+++ JavaBaseProject/src/main/java/com/wk/JMX/ch7_modelMBean/ModelClass.java
@@ -0,0 +1,76 @@
+package com.wk.JMX.ch7_modelMBean;
+
+import com.wk.JMX.ch3.util.RMIClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.management.*;
+import javax.management.modelmbean.ModelMBeanInfo;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class ModelClass implements Serializable{
+    private static Logger log = LoggerFactory.getLogger(ModelClass.class);
+    private String attribute = "My Attribute";
+
+    public String getMyAttribute(){
+        log.info("Returning attribute to MBean");
+        return attribute;
+    }
+
+    public void printAttribute(){
+        log.info("print attribute:"+attribute);
+    }
+
+    public static void main(String[] args) throws MalformedObjectNameException, NotCompliantMBeanException, ReflectionException,
+                                        MBeanException, InstanceAlreadyExistsException, IOException, InstanceNotFoundException {
+        ModelClass modelClass = new ModelClass();
+        ModelMBeanInfoBuilder builder = new ModelMBeanInfoBuilder();
+        // 创建ModelMBean
+        Descriptor attributeDesc = builder.buildAttributeDescriptor("attribute", null, "always",
+                "10",null, "getAttribute", null, "10");
+
+        builder.addModelMBeanAttribute("attribute","java.lang.String",true,false,false,"",attributeDesc);
+
+        Descriptor operationDescriptor = builder.buildOperationDescriptor("getMyAttribute", null, "getter",
+                null, null,"com.wk.JMX.ch7_modelMBean.ModelClass", "10");
+        builder.addModelMBeanMethod("getMyAttribute",null,null,null,"",
+                "java.lang.String", MBeanOperationInfo.INFO,operationDescriptor);
+
+        Descriptor printOperation = builder.buildOperationDescriptor("printAttribute", null, "operation",
+                null, null,"com.wk.JMX.ch7_modelMBean.ModelClass", "10");
+        builder.addModelMBeanMethod("printAttribute",null,null,null,
+                "","void",MBeanOperationInfo.ACTION,printOperation);
+
+        Descriptor mbeanDesc = builder.buildMBeanDescriptor("modeledClass", "", "always", "10",
+                ".", "ModeledClass", null, null);
+
+        ModelMBeanInfo modelMBeanInfo = builder.buildModelMBeanInfo(mbeanDesc);
+
+        MBeanServerConnection connection = RMIClientFactory.getConnection();
+        ObjectName objectName = new ObjectName("JMXBookAgent:name=Modeled");
+        connection.createMBean("javax.management.modelmbean.RequiredModelMBean",objectName);
+
+
+        String[] sig = {"java.lang.Object","java.lang.String"};
+        Object[] params = { modelClass,"ObjectReference" };
+
+        /**
+         *  在这里调用方法 注入时  报错；
+         *  javax.management.ServiceNotFoundException: Operation setManagedResource not in ModelMBeanInfo
+         *
+         *  这里调用方式不对 ~~~ ????
+         */
+        // connection.invoke(objectName,"setManagedResource",params,sig);
+        sig = new String[1];
+        sig[0] = "javax.management.modelmbean.ModelMBeanInfo";
+        params = new Object[1];
+        params[0] = modelMBeanInfo;
+        /**
+         * javax.management.ServiceNotFoundException: Operation setModelMBeanInfo not in ModelMBeanInfo
+         */
+        connection.invoke(objectName,"setModelMBeanInfo",params,sig);
+
+        // connection.invoke(objectName,"store",null,null);
+    }
+}
---
+++ JavaBaseProject/src/main/java/com/wk/JMX/ch7_modelMBean/ModelMBeanInfoBuilder.java
@@ -0,0 +1,157 @@
+package com.wk.JMX.ch7_modelMBean;
+
+import javax.management.Descriptor;
+import javax.management.MBeanParameterInfo;
+import javax.management.modelmbean.*;
+import java.lang.reflect.Constructor;
+import java.util.Hashtable;
+import java.util.Vector;
+
+public class ModelMBeanInfoBuilder {
+    protected Hashtable attributes = new Hashtable();
+    protected Hashtable notifications = new Hashtable();
+    protected Hashtable constructors = new Hashtable();
+    protected Hashtable operations = new Hashtable();
+
+    public void addModelMBeanMethod(String name, String[] paramTypes, String[] paramNames, String[] paramDescs,
+                                    String description, String rtype, int type, Descriptor desc){
+        MBeanParameterInfo[] params = null;
+        if (paramTypes != null){
+            params = new MBeanParameterInfo[paramTypes.length];
+
+            for (int i = 0; i < paramTypes.length; i++) {
+                params[i] = new MBeanParameterInfo(paramNames[i],paramTypes[i],paramDescs[i]);
+            }
+        }
+        operations.put(name,new ModelMBeanOperationInfo(name,description,params,rtype,type,desc));
+    }
+
+    public void addModelMBeanNotification(String[] type,String className,String description,Descriptor desc){
+        notifications.put(className,new ModelMBeanNotificationInfo(type,className,description,desc));
+    }
+
+    public void addModelMBeanAttribute(String name,String type,boolean read,boolean write,boolean is,
+                                       String description,Descriptor desc){
+        attributes.put(name,new ModelMBeanAttributeInfo(name,type,description,read,write,is,desc));
+    }
+
+    public void addModelMBeanConstructor(Constructor c,String description,Descriptor desc){
+        constructors.put(c,new ModelMBeanConstructorInfo(description,c,desc));
+    }
+
+    public ModelMBeanInfo buildModelMBeanInfo(Descriptor desc){
+        ModelMBeanOperationInfo[] ops = new ModelMBeanOperationInfo[operations.size()];
+        copyInfo(ops,operations);
+
+        ModelMBeanAttributeInfo[] attrs = new ModelMBeanAttributeInfo[attributes.size()];
+        copyInfo(attrs,attributes);
+
+        ModelMBeanConstructorInfo[] constrs = new ModelMBeanConstructorInfo[constructors.size()];
+        copyInfo(constrs,constructors);
+
+        ModelMBeanNotificationInfo[] notifs = new ModelMBeanNotificationInfo[this.notifications.size()];
+        copyInfo(notifs,notifications);
+
+        ModelMBeanInfoSupport modelMBeanInfoSupport = new ModelMBeanInfoSupport("", "description",
+                            attrs, constrs, ops, notifs, desc);
+
+        return modelMBeanInfoSupport;
+    }
+
+    public Descriptor buildAttributeDescriptor(String name, String displayName, String persistPolicy,
+                                               String persistPeriod, Object defaultValue, String getter,
+                                               String setter, String currency){
+        DescriptorSupport desc = new DescriptorSupport();
+        if (name != null)
+            desc.setField("name",name);
+        desc.setField("descriptorType","attribute");
+        if (displayName != null)
+            desc.setField("displayName",displayName);
+        if (getter != null)
+            desc.setField("getMethod",getter);
+        if (setter != null)
+            desc.setField("setMethod",setter);
+        if (currency != null)
+            desc.setField("currencyTimeLimit",currency);
+        if (persistPolicy != null)
+            desc.setField("persistPolicy",persistPolicy);
+        if (persistPeriod != null)
+            desc.setField("persistPeriod",persistPeriod);
+        if (defaultValue != null)
+            desc.setField("default",defaultValue);
+
+        return desc;
+    }
+
+    public Descriptor buildOperationDescriptor(String name,String displayName,String role,Object targetObject,
+                                               Object targetType,String ownerClass,String currency){
+        DescriptorSupport desc = new DescriptorSupport();
+        if (name != null)
+            desc.setField("name",name);
+        desc.setField("descriptorType","operation");
+        if (displayName != null)
+            desc.setField("displayName",displayName);
+        if (role != null)
+            desc.setField("role",role);
+        if (targetObject != null)
+            desc.setField("targetObject",targetObject);
+        if (targetType != null)
+            desc.setField("taretType",targetType);
+        if (ownerClass != null)
+            desc.setField("class",ownerClass);
+        if (currency != null)
+            desc.setField("currencyTimeLimit",currency);
+
+        return desc;
+    }
+
+    public Descriptor buildMBeanDescriptor(String name,String displayName,String persistPolicy,String persistPeriod,
+                                           String persistLocation,String persistName,String log,String logFile){
+        DescriptorSupport desc = new DescriptorSupport();
+        if (name != null)
+            desc.setField("name",name);
+        desc.setField("descriptorType","mbean");
+        if (displayName != null)
+            desc.setField("displayName",displayName);
+        if (persistLocation != null)
+            desc.setField("persistLocation",persistLocation);
+        if (persistName != null)
+            desc.setField("persistName",persistName);
+        if (log != null)
+            desc.setField("log",log);
+        if (persistPolicy != null)
+            desc.setField("persistPolicy",persistPolicy);
+        if (persistPeriod != null)
+            desc.setField("persistPeriod",persistPeriod);
+        if (logFile != null)
+            desc.setField("logFile",logFile);
+
+        return desc;
+    }
+
+    private void copyInfo(Object[] array,Hashtable table){
+        Vector vector = new Vector(table.values());
+        vector.copyInto(array);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
