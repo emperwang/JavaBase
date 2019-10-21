e06cc16362fa8a71fde0c195617dcef69d35feac
emperwang <544094478@qq.com>
2019-10-21 15:12:13 +0800

    rxJava 2

--- JavaBaseProject/src/main/java/com/wk/rxjava/BaseDemo.java
+++ JavaBaseProject/src/main/java/com/wk/rxjava/BaseDemo.java
@@ -43,12 +43,49 @@ public class BaseDemo {
                 System.out.println("Complete");
             }
         };
-
+        // 事件订阅
         observable.subscribe(observer);
+    }
 
+    public static void demostration2(){
+        // 链式结构
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> observableEmitter) throws Exception {
+                observableEmitter.onNext(1);
+                observableEmitter.onNext(2);
+                observableEmitter.onNext(3);
+                observableEmitter.onNext(4);
+                observableEmitter.onNext(5);
+                observableEmitter.onNext(6);
+                observableEmitter.onComplete();
+            }
+        })
+         .subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable disposable) {
+                System.out.println("begin subscribe event");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("onNext :"+integer);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError :"+throwable.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
     }
 
     public static void main(String[] args) {
-        demostration1();
+        //demostration1();
+        demostration2();
     }
 }
