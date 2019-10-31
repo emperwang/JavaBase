b4d04fa9f63800775331845ba4585cfd63a2b4c6
emperwang <544094478@qq.com>
2019-10-31 15:38:58 +0800

    guava

--- JavaBaseProject/src/main/java/com/wk/guava/cache/CacheUtil.java
+++ JavaBaseProject/src/main/java/com/wk/guava/cache/CacheUtil.java
@@ -1,9 +1,94 @@
 package com.wk.guava.cache;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class CacheUtil {
-
-
-    public static void main(String[] args) {
-        
+    private static Employee getFromDataBase(String empId){
+        Employee e1 = new Employee("Mahesh", "Finance", "100");
+        Employee e2 = new Employee("Rohan", "IT", "103");
+        Employee e3 = new Employee("Sohan", "Admin", "110");
+        Map<String,Employee> data = new HashMap<>();
+        data.put("100",e1);
+        data.put("103",e2);
+        data.put("110",e3);
+        System.out.println("Database hit for :" + empId);
+        return data.get(empId);
     }
+
+    public static void CacheUtil() throws ExecutionException {
+        LoadingCache<String, Employee> cache = CacheBuilder.newBuilder()
+                .maximumSize(100)    // 设置大小  以及 过去的策略
+                .expireAfterAccess(30, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, Employee>() {
+                    @Override
+                    public Employee load(String empId) throws Exception {
+                        return getFromDataBase(empId);
+                    }
+                });
+        System.out.println("101 : " + cache.get("100"));
+        System.out.println("103 : " + cache.get("100"));
+        System.out.println("121 : " + cache.get("100"));
+        System.out.println(" second call ,load from cache .......");
+        System.out.println("101 : " + cache.get("100"));
+        System.out.println("103 : " + cache.get("100"));
+        System.out.println("121 : " + cache.get("100"));
+    }
+
+    public static void main(String[] args) throws ExecutionException {
+        CacheUtil();
+    }
+
+
+    static  class Employee{
+        String name;
+        String dept;
+        String empId;
+
+        public Employee(String name, String dept, String empId) {
+            this.name = name;
+            this.dept = dept;
+            this.empId = empId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDept() {
+            return dept;
+        }
+
+        public void setDept(String dept) {
+            this.dept = dept;
+        }
+
+        public String getEmpId() {
+            return empId;
+        }
+
+        public void setEmpId(String empId) {
+            this.empId = empId;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "name='" + name + '\'' +
+                    ", dept='" + dept + '\'' +
+                    ", empId='" + empId + '\'' +
+                    '}';
+        }
+    }
+
 }
--- JavaBaseProject/src/main/java/com/wk/guava/string/StringUtil.java
+++ JavaBaseProject/src/main/java/com/wk/guava/string/StringUtil.java
@@ -1,4 +1,61 @@
 package com.wk.guava.string;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class StringUtil {
+
+    public static void StrUtil(){
+        List<Integer> lists = Arrays.asList(1, 2, 3, 4, 5, 6);
+        String join = Joiner.on(".").skipNulls().join(lists.toArray());
+        System.out.println(join);
+        String s = Joiner.on("..").appendTo(new StringBuilder(), lists.toArray()).append(100).append(200).toString();
+        System.out.println(s);
+    }
+
+    public static void Spliterutil(){
+        Iterable<String> split = Splitter.on(",")
+                .trimResults()      // 去除左右的空格
+                .omitEmptyStrings() //  消除空字符串
+                .split("the ,quick, , brown         , fox,              jumps, over, the, lazy, little dog");
+        split.forEach(val -> {
+            System.out.print(" " + val);
+        });
+    }
+
+    public static void MatcherUtil(){
+        System.out.println(CharMatcher.ascii().retainFrom("mahesh123"));
+        System.out.println(CharMatcher.whitespace().trimAndCollapseFrom("     Mahesh     Parashar ", ' '));
+
+        System.out.println(CharMatcher.anyOf("123456789").retainFrom("test123"));
+        System.out.println(CharMatcher.noneOf("123").retainFrom("test123"));
+        System.out.println(CharMatcher.none().retainFrom("tesdt123"));
+        System.out.println(CharMatcher.anyOf("123").or(CharMatcher.anyOf("789")).retainFrom("test789"));
+    }
+
+    /** 格式              example
+     * LOWER_CAMEL      lowerCamel
+     * LOWER_HYPHEN     lower-hyphen
+     * LOWER_UNDERSCORE     lower_underscore
+     * UPPER_CAMEL          UpperCamel
+     * UPER_UNDERSCORE      UPPER_UNDERSCORE
+     */
+    // 格式转换
+    public static void CaseFormatUtil(){
+        System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL,"test-data"));
+        System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL,"test_data"));
+        System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_UNDERSCORE,"test_data"));
+    }
+
+    public static void main(String[] args) {
+//        StrUtil();
+//        Spliterutil();
+//        MatcherUtil();
+        CaseFormatUtil();
+    }
 }
