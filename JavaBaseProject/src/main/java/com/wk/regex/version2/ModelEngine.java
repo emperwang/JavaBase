323ee2cb8076d9c5fb871ffd38c36337c40157da
emperwang <544094478@qq.com>
2019-11-06 18:32:57 +0800

    engine

--- JavaBaseProject/src/main/java/com/wk/regex/version2/ModelEngine.java
+++ JavaBaseProject/src/main/java/com/wk/regex/version2/ModelEngine.java
@@ -12,7 +12,31 @@ public class ModelEngine {
         map.put("name","zhangsan");
         map.put("code","12345345");
         engineOne(template,map);
+        System.out.println(" ====================================================== ");
+        String templates = "收到活动告警，告警标题：$alarmTitle，告警级别：$origSeverity，告警发生时间：$eventTime ," +
+                "   please check ....";
+        Map<String,Object> res = new HashMap<>();
+        res.put("alarmTitle","alarmTitleVal");
+        res.put("origSeverity","origSeverityVal");
+        res.put("eventTime","eventTimeVal");
+        enginTwo(templates,res);
+    }
 
+    public static void enginTwo(String strs,Map<String,Object> res){
+        String regex = "\\$\\w+";
+        Pattern engine = Pattern.compile(regex);
+        Matcher matcher = engine.matcher(strs);
+        StringBuffer builder = new StringBuffer();
+        while (matcher.find()){
+            String key = matcher.group();
+            System.out.println("key = " + key);
+            Object values = res.get(key.substring(1));
+            String val = Matcher.quoteReplacement(values.toString());
+            matcher.appendReplacement(builder,val);
+            System.out.println(builder.toString());
+        }
+        matcher.appendTail(builder);
+        System.out.println("last Result : " + builder.toString());
     }
 
     public static void engineOne(String template,Map<String,Object> params){
