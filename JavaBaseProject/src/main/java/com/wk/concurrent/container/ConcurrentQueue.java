8f7e34dd67951a5ca5f68a519369bc693969e797
emperwang <544094478@qq.com>
2019-11-15 14:39:14 +0800

    concurrentContainer

--- JavaBaseProject/src/main/java/com/wk/concurrent/container/ConcurrentQueue.java
+++ JavaBaseProject/src/main/java/com/wk/concurrent/container/ConcurrentQueue.java
@@ -1,11 +1,32 @@
 package com.wk.concurrent.container;
 
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ConcurrentQueue {
     public static void main(String[] args) {
+        //concurrentLinkedQueueDemo();
+        concurrentLinkedDeque();
+    }
+
+    private static void concurrentLinkedDeque(){
+        ConcurrentLinkedDeque<String> deque = new ConcurrentLinkedDeque<>();
+
+        deque.add("123");
+
+        deque.addFirst("qwe");
+
+        String s = deque.pollFirst();
+
+    }
+
+    private static void concurrentLinkedQueueDemo() {
         Queue<String> strs = new ConcurrentLinkedQueue<>();
+        strs.offer("aaa");
+        strs.offer("aaa22");
+        strs.poll();
+        strs.poll();
         for (int i=0; i<10;i++){
             strs.offer("a"+i);
         }
