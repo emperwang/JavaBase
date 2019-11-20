d641177d3b1538884512fb4e657fb57101b29f32
emperwang <544094478@qq.com>
2019-11-20 11:33:43 +0800

    solr log4j.properties

--- SolrJ/src/main/java/com/wk/SolrJService.java
+++ SolrJ/src/main/java/com/wk/SolrJService.java
@@ -1,7 +1,35 @@
 package com.wk;
 
-public class SolrJService {
-    public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.junit.Test;
 
+@Slf4j
+public class SolrJService {
+    private String URL = "http://localhost:8080/solr";
+
+
+    @Test
+    public void searchAll() throws SolrServerException {
+        HttpSolrServer solrServer = new HttpSolrServer(URL);
+        SolrQuery query = new SolrQuery();
+        // 输入查询条件
+        query.setQuery("*:*");
+        // exceute query
+        QueryResponse response = solrServer.query(query);
+        SolrDocumentList solrDocuments = response.getResults();
+        long totalDoc = solrDocuments.getNumFound();
+        log.info("find num:{}",totalDoc);
+        for (SolrDocument document : solrDocuments) {
+            log.info("id = {}",document.get("id"));
+            log.info("name = {}",document.get("pname"));
+            log.info("age = {}",document.get("page"));
+            log.info("address = {}",document.get("paddress"));
+        }
     }
 }
