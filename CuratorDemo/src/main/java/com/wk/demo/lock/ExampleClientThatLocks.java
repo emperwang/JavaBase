7d5b34de4938294ae480e072233703c01b381b10
emperwang <544094478@qq.com>
2019-12-02 19:19:14 +0800

    curator

--- CuratorDemo/src/main/java/com/wk/demo/lock/ExampleClientThatLocks.java
+++ CuratorDemo/src/main/java/com/wk/demo/lock/ExampleClientThatLocks.java
@@ -1,16 +1,17 @@
 package com.wk.demo.lock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.utils.CloseableUtils;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class ExampleClientThatLocks {
     private InterProcessMutex lock;
     private FakeLimitedResources resources;
@@ -26,11 +27,12 @@ public class ExampleClientThatLocks {
         if(!lock.acquire(time,timeUnit)){
             throw new IllegalStateException(clientName+" could not acquire a lock");
         }
+        log.info(clientName + " has a lock");
         System.out.println(clientName + " has a lock");
-
         try{
             resources.use();
         }finally {
+            log.info(clientName+" releasing the lock");
             System.out.println(clientName+" releasing the lock");
             lock.release();
         }
@@ -46,13 +48,33 @@ public class ExampleClientThatLocks {
         int QTY = 5;
         int REPETITIONS = QTY * 5;
         String PATH = "/example/locks";
-        String CONNECT_ADDR = "192.168.72.15:2181";
+        String CONNECT_ADDR = "localhost:2181";
         FakeLimitedResources fakeLimitedResources = new FakeLimitedResources();
         ExecutorService executorService = Executors.newFixedThreadPool(QTY);
 
-            for (int i = 0; i < QTY; i++) {
+            for (int i = 0; i < 1; i++) {
                 int index = 1;
-                Callable<Void> task = () -> {
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        CuratorFramework curator = CuratorFrameworkFactory.newClient(CONNECT_ADDR,
+                                new RetryNTimes(3, 1000));
+                        curator.start();
+                        try {
+                            ExampleClientThatLocks exampleClientThatLocks = new ExampleClientThatLocks(curator, PATH, fakeLimitedResources,
+                                    "Client" + index);
+
+                            for (int j = 0; j < REPETITIONS; j++) {
+                                exampleClientThatLocks.doWork(10, TimeUnit.SECONDS);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            CloseableUtils.closeQuietly(curator);
+                        }
+                    }
+                };
+                /*Callable<Void> task = () -> {
                     CuratorFramework curator = CuratorFrameworkFactory.newClient(CONNECT_ADDR,
                             new RetryNTimes(3, 1000));
                     curator.start();
@@ -67,14 +89,16 @@ public class ExampleClientThatLocks {
                        CloseableUtils.closeQuietly(curator);
                    }
                     return null;
-                };
-                executorService.submit(task);
+                };*/
+                executorService.execute(runnable);
             }
-        executorService.shutdown();
+
         try {
-            executorService.awaitTermination(10,TimeUnit.SECONDS);
+            executorService.awaitTermination(Long.MAX_VALUE,TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }finally {
+            executorService.shutdown();
         }
     }
 }
--- CuratorDemo/src/main/java/com/wk/demo/lock/FakeLimitedResources.java
+++ CuratorDemo/src/main/java/com/wk/demo/lock/FakeLimitedResources.java
@@ -1,16 +1,18 @@
 package com.wk.demo.lock;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+@Slf4j
 public class FakeLimitedResources {
     private final AtomicBoolean inUse = new AtomicBoolean(false);
     public void use(){
         if(!inUse.compareAndSet(false,true)){
             throw new IllegalStateException("needs to be use by one client at a time");
         }
-
         try {
-            System.out.println("fakeLimiteResources in use");
+            log.info("************************************fakeLimiteResources in use*****************************");
+            System.out.println("************************************fakeLimiteResources in use*****************************");
             Thread.sleep((long) (3*Math.random()));
         } catch (InterruptedException e) {
             e.printStackTrace();
