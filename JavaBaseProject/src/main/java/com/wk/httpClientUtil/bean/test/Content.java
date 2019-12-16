6ee9ccc5613fb2066c169d74bd8e25e39171fbfe
emperwang <544094478@qq.com>
2019-12-16 09:52:11 +0800

    add test bean

---
+++ JavaBaseProject/src/main/java/com/wk/httpClientUtil/bean/test/Content.java
@@ -0,0 +1,12 @@
+package com.wk.httpClientUtil.bean.test;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Content {
+    String content;
+}
