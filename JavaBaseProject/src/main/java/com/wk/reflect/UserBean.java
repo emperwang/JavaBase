d48cf2f7a10dda29d7a133165a0b5580029d45b3
emperwang <544094478@qq.com>
2020-06-05 16:50:37 +0800

    get inner class by reflect

---
+++ JavaBaseProject/src/main/java/com/wk/reflect/UserBean.java
@@ -0,0 +1,23 @@
+package com.wk.reflect;
+
+public class UserBean {
+    private String name;
+    private String addr;
+
+
+    class User1{
+        private String name1;
+        private String addr1;
+    }
+
+    public static class User2{
+        private String name2;
+        private String addr3;
+    }
+
+    private static class User3{
+        private String name3;
+        private String addr3;
+    }
+
+}
