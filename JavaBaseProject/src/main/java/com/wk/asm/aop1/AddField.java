0d3078d990db0f8fb6054f83fa8f15f01323d5f6
emperwang <544094478@qq.com>
2020-06-30 17:47:53 +0800

    comment in asm

--- JavaBaseProject/src/main/java/com/wk/asm/aop1/AddField.java
+++ JavaBaseProject/src/main/java/com/wk/asm/aop1/AddField.java
@@ -9,7 +9,27 @@ import org.objectweb.asm.ClassWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+/*
+class 文件结构
+ClassFile {
+    u4 magic;
+    u2 minor_version;
+    u2 major_version;
+    u2 constant_pool_count;
+    cp_info constant_pool[constant_pool_count-1];
+    u2 access_flags;
+    u2 this_class;
+    u2 super_class;
+    u2 interfaces_count;
+    u2 interfaces[interfaces_count];
+    u2 fields_count;
+    field_info fields[fields_count];
+    u2 methods_count;
+    method_info methods[methods_count];
+    u2 attributes_count;
+    attribute_info attributes[attributes_count];
+}
+ */
 public class AddField {
     private Class<?> clazz;
     private ClassReader cr;
@@ -29,7 +49,14 @@ public class AddField {
         cw = new ClassWriter(cr,ClassWriter.COMPUTE_MAXS);
         cv = new AddFieldAdapter(cw, Opcodes.ACC_PUBLIC,fieldName,fieldDesc);
     }
-
+    /*
+       ClassReader用于读取 class文件,其具体的读取方法会委托与内部的一个ClassVisitor
+       而从这里 cr.accept(cv,ClassReader.SKIP_DEBUG)可以看到,在这里委托的具体访问的类为AddFieldAdapter
+       而AddFieldAdapter是ClassVisitor的子类,其中ClassVisitor是一个抽象类,其读取方法也会委托到内部的一个类
+       new AddFieldAdapter(cw, Opcodes.ACC_PUBLIC,fieldName,fieldDesc)由此可知,ClassVisitor内部委托的读取类应该是ClassWriter
+       而AddFieldAdapter重写了visitField 和 visitEnd 方法, 也就是说当执行visitField 和 visitEnd方法时,会具体执行AddFieldAdapter
+       的方法,不会执行ClassWriter的方法; 当时访问class的其他方法时,就会委托到 ClassWriter
+     */
     public void writeByteCode() throws IOException {
         cr.accept(cv,ClassReader.SKIP_DEBUG);
         byte[] bytes = cw.toByteArray();
