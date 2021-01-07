ab1b6fbd8bf60d9496178d025f23c21d6aae56ab
emperwang <544094478@qq.com>
2021-01-07 18:47:12 +0800

    lee code

--- LeeCode/src/main/java/com/wk/Solution_romToInt.java
+++ LeeCode/src/main/java/com/wk/Solution_romToInt.java
@@ -1,5 +1,8 @@
 package com.wk;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: wk
  * @Date: 2021/1/6 18:06
@@ -8,14 +11,14 @@ package com.wk;
 public class Solution_romToInt {
     /*
     罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
-    字符          数值
-    I             1
-    V             5
-    X             10
-    L             50
-    C             100
-    D             500
-    M             1000
+        字符          数值
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
 
     例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
     通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，
@@ -28,10 +31,86 @@ public class Solution_romToInt {
     示例 1:
     输入: "III"
     输出: 3
-
      */
+    /* 10 10 5 1
+        X X V I     26
+       10 10 -1 5
+        X X I V     24
+        可以每次获取两个值,如果前面的值比后面的小,则进行的是减法,否则为加法;如果是最后一个值,则表示加法
+     */
+    // 空间 O(1)  时间 O(n)
     public int romanToInt(String s){
+        if (s == null || s.length() <=0){
+            return 0;
+        }
+        int res = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i=1;i<=s.length()-1; i++){
+           int num = getValue(s.charAt(i));
+            if (preNum < num){
+                res -= preNum;
+            }else{
+                res += preNum;
+            }
+            preNum = num;
+        }
+        res += preNum;
+        return res;
+    }
 
-        return 0;
+    private int getValue(char s){
+        switch (s){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return -1;
+    }
+    // 时间: O(n)  空间O(1)
+    public int romToInt2(String s){
+        if (s == null || s.length() <= 0){
+            return 0;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int preNum = map.get(s.charAt(0));
+        int res = 0;
+        for (int i=1; i<s.length();i++){
+            int num = map.get(s.charAt(i));
+            if (preNum < num){
+                res -= preNum;
+            }else{
+                res += preNum;
+            }
+            preNum = num;
+        }
+        res+=preNum;
+        return res;
+    }
+    public static void main(String[] args) {
+        final Solution_romToInt romToInt = new Solution_romToInt();
+        System.out.println(romToInt.getValue('I'));
+        System.out.println(romToInt.romanToInt("XXVI"));    // 26
+        System.out.println(romToInt.romanToInt("XXIV"));    // 24
+        //
+        System.out.println(romToInt.romToInt2("XXVI"));    // 26
+        System.out.println(romToInt.romToInt2("IV"));    //
     }
 }
